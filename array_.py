69c4de7d90049f8860549c40e92456f6303952cc
suvojitd79@gmail.com <suvojitd79@gmail.com>
2019-07-07 14:09:16 +0530

    pairsum added

--- array_.py
+++ array_.py
@@ -1,11 +1,11 @@
 
 '''
 given two strings. checks if they're anagrams. you can ignore spaces & capitalization
-'''
 
-# O(nlogn) - sort
-# O(n) time + O(n) space - count
-# O(n) time + O(1) space -bit
+O(nlogn) - sort
+O(n) time + O(n) space - count
+O(n) time + O(1) space -bit
+'''
 
 
 def anagrams(s1, s2, type='sort'):
@@ -22,3 +22,32 @@ def anagrams(s1, s2, type='sort'):
         for i in range(len(s1)):
             ans = ans ^ ord(s1[i]) ^ ord(s2[i])
         return ans == 0
+
+
+'''
+given a list of integers and an integer(k), return the number of pairs such that each pair sum to the given value k
+'''
+
+
+def pairsum(l, k):
+    if len(l) == 0:
+        return
+    d_ = dict()
+    _ = []
+    for v in l:
+        if v in d_:
+            d_.update({v: d_[v]+1})
+        else:
+            d_[v] = 1
+    for v in l:
+        if k-v in d_ and ((v == k-v and d_[k-v] > 1) or (v != k-v and d_[k-v] > 0)):
+            _.append([v, k-v])
+
+    for __ in _:
+        if _.count(__) > 1:
+            _.remove(__)
+        elif [__[1], __[0]] in _:
+            _.remove([__[1], __[0]])
+    return _, len(_)
+
+
--- test_code.py
+++ test_code.py
@@ -6,3 +6,9 @@ def test_anagrams():
     assert array_.anagrams('d o g', 'dog', type='bit') == True
     assert array_.anagrams(
         'geeksforgeeks', 'forgeeksgeeks', type='bit') == True
+
+
+def test_pairsum():
+    assert array_.pairsum([1, 3, 2, 2], 4)[1] == 2
+    assert array_.pairsum([1, 4, 45, 6, 10, 8], 16)[1] == 1
+    assert array_.pairsum([1, 3, 23, 9, 0], 9)[1] == 1
